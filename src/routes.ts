fa3f7786a131f8a266722f5101b95058803e4658
Bhargav Panth <bhargavrpanth@gmail.com>
2020-05-04 23:08:47 +0000

    WIP - will adapt the server to fetch data from different source

--- src/routes.ts
+++ src/routes.ts
@@ -1,7 +1,9 @@
 // Routes are unreadable - move to a different implementation
 module.exports = {
 	'/': require('./controller/index_controller.js'),
+	// deprecate this route
 	'/poll/bus/data': require('./controller/bus_controller.js'),
+	// deprecate this route
 	'/poll/bikes/data': require('./controller/bikes_controller.js'),
 	'/wait/time': require('./controller/waiting_time_controller.js'),
 	'/beacon': require('./controller/beacon_controller.js')
