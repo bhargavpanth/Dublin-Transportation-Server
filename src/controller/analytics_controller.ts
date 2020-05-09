f8e53ab5276c3867e8e61ce9d9f29dbabf81cd66
Bhargav Panth <bhargavrpanth@gmail.com>
2020-05-09 00:11:35 +0000

    WIP - adding analytics controller

--- src/controller/analytics_controller.ts
+++ src/controller/analytics_controller.ts
@@ -1,1 +1,9 @@
 import express from 'express'
+import schema from '../model'
+
+const { analytics_schema } = schema()
+let router = express.Router()
+
+const index = (app, route) => {
+    // Add socket.io
+}
