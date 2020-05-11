71fb7d8c223788a4d8fffb00cb82a365c4f2111b
Bhargav Panth <bhargavrpanth@gmail.com>
2020-05-11 22:11:43 +0000

    WIP

--- src/controller/analytics_controller.ts
+++ src/controller/analytics_controller.ts
@@ -1,12 +1,21 @@
 import express from 'express'
+import http from 'http'
+import socketio from 'socket.io'
 import schema from '../model'
 
 const { analytics_schema } = schema()
 let router = express.Router()
 
+const io = socketio
+
 const index = (app, route) => {
     // Add socket.io
     router.get('/', (req, res) => {
         // might make sense to return a JWT token
+        // and req params could contain the reverse geo-code
+        // of the location where the users are expecting the
+        // analytics from
     })
+
+
 }
